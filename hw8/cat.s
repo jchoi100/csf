1ffc8bd673d1d859a24f2e77872a88e9e4712508
Joon Hyuck Choi <joonhyuck.choi92@gmail.com>
2016-04-25 22:21:19 -0400

    ex3 temps

---
+++ hw8/cat.s
@@ -0,0 +1,32 @@
+	.file	"cat.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB2:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	jmp	.L2
+.L3:
+	movl	-4(%rbp), %eax
+	movl	%eax, %edi
+	call	putchar
+.L2:
+	call	getchar
+	movl	%eax, -4(%rbp)
+	cmpl	$-1, -4(%rbp)
+	jne	.L3
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.2.1-22ubuntu2) 5.2.1 20151010"
+	.section	.note.GNU-stack,"",@progbits
