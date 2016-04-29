c5112f7b73d455040b198772f2ac4fb09ee14336
Joon Hyuck Choi <joonhyuck.choi92@gmail.com>
2016-04-29 16:41:05 -0400

    submitted

---
+++ hw8/test/cat.s
@@ -0,0 +1,41 @@
+	.file	"cat.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	leal	4(%esp), %ecx
+	.cfi_def_cfa 1, 0
+	andl	$-16, %esp
+	pushl	-4(%ecx)
+	pushl	%ebp
+	.cfi_escape 0x10,0x5,0x2,0x75,0
+	movl	%esp, %ebp
+	pushl	%ecx
+	.cfi_escape 0xf,0x3,0x75,0x7c,0x6
+	subl	$20, %esp
+	jmp	.L2
+.L3:
+	subl	$12, %esp
+	pushl	-12(%ebp)
+	call	putchar
+	addl	$16, %esp
+.L2:
+	call	getchar
+	movl	%eax, -12(%ebp)
+	cmpl	$-1, -12(%ebp)
+	jne	.L3
+	movl	$0, %eax
+	movl	-4(%ebp), %ecx
+	.cfi_def_cfa 1, 0
+	leave
+	.cfi_restore 5
+	leal	-4(%ecx), %esp
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.2.1-22ubuntu2) 5.2.1 20151010"
+	.section	.note.GNU-stack,"",@progbits
